d2aca232e291b1ffbdb2685a5a7da8dc5c60746b
sirtimtim <torubarovart@gmail.com>
2022-02-26 20:53:26 -0500

    worked on app.py flask functionality

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def index():
+    flash("what's your name?")
+    return render_template("index.html")
+
